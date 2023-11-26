672bb44f6b73703713a850fd0e475c5a246aac12
antelava11 <nikoantelavaz13@gmail.com>
2023-11-26 20:56:03 +0300

    დავალება 7

---
+++ Calculator/javascript/calcUlator.js
@@ -0,0 +1,20 @@
+let outputScreen = document.getElementById("output-screen")
+
+function display(num){
+    outputScreen.value += num;
+}
+function Calculate() {
+    try{
+        outputScreen.value = eval(outputScreen.value)
+
+    }
+    catch(err)
+    {
+        alert("Invalide")
+        
+    }
+    
+}
+function Clear(){
+    outputScreen.value = "";
+}
